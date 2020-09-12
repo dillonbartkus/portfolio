af3c2d6833148951ea7b91e7bbf75e102323b500
unknown <dillonbartkus@gmail.com>
2020-09-12 15:14:22 -0400

    updated screenshots

--- src/Log.js
+++ src/Log.js
@@ -7,8 +7,6 @@ import four from './images/log-screenshot4.png'
 import five from './images/log-screenshot5.png'
 import six from './images/log-screenshot6.png'
 import seven from './images/log-screenshot7.png'
-import eight from './images/log-screenshot8.png'
-import nine from './images/log-screenshot9.png'
 
 export default function() {
 
@@ -30,14 +28,12 @@ export default function() {
           static // default false
         >
           <div className="carousel-cell"> <img src = {one} alt = '' /> </div>
-          <div className="carousel-cell portrait"><img src = {two} alt = '' /> </div>
-          <div className="carousel-cell portrait"><img src = {three} alt = '' /> </div>
+          <div className="carousel-cell"><img src = {two} alt = '' /> </div>
+          <div className="carousel-cell"><img src = {three} alt = '' /> </div>
           <div className="carousel-cell"><img src = {four} alt = '' /> </div>
-          <div className="carousel-cell"><img src = {five} alt = '' /> </div>
+          <div className="carousel-cell portrait"><img src = {five} alt = '' /> </div>
           <div className="carousel-cell"><img src = {six} alt = '' /> </div>
           <div className="carousel-cell"><img src = {seven} alt = '' /> </div>
-          <div className="carousel-cell"><img src = {eight} alt = '' /> </div>
-          <div className="carousel-cell"><img src = {nine} alt = '' /> </div>
 
         </Flickity>
         )
